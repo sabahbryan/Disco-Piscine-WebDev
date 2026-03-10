9a622e163f50d251b92dc37b31846d7466c752e3
Bryan <89350478+sabahbryan@users.noreply.github.com>
2026-03-10 23:34:32 +0800

    second commit
    Work done on 2nd day

--- cell00/ex04/argv.sh
+++ cell00/ex04/argv.sh
@@ -5,11 +5,13 @@ if [ $# -eq 0 ] ;then
 elif [ $# -ge 4 ];then
     echo "Less than four arguments"
 else
-    #echo "Arguments: $#"
+    for word in $@; do echo $word; done
+fi
+
+
+#echo "Arguments: $#"
     #count=1
     #while [ $count -lt 4 ]; do # less than 4
     #    echo $@
     #    ((count++))
     #done
-    for word in $@; do echo "$word"; done
-fi
